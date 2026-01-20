263ebb90c78d68633506ff0edfa8f3a294d1cf49
chedan2038 <chedan2038@gmail.com>
2026-01-20 21:13:25 +0300

    main init

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-
-from game_map.game_map import map_generator
+from enemy.enemy import Enemy
+from player.player import Player
 
 
 class Game:
+    # rooms = map_generator()
+    print('\n')
+    rooms = ['St', 'E', ' ', 'Ex']
+    # print(rooms)
 
+    player = Player()
+    player.generate_player()
+    # print(player.entity.name)
 
-    new_map = map_generator()
+    for r in rooms:
+        if r == 'E':
+            enemy = Enemy()
+            enemy.generate_enemy()
+            print(enemy.entity.name)
 
-    print(new_map)
+        elif r == ' ':
+            print('Пусто')
+        elif r == 'St':
+            pass
+            # print('Вход')
+        elif r == 'Ex':
+            pass
+            # print('Выход')
