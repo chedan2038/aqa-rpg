f4e59263cd3c808cff31bfde75c1b7d7b77008f6
chedan2038 <chedan2038@gmail.com>
2026-01-20 23:50:06 +0300

    auto fight concept

--- player/player.py
+++ player/player.py
@@ -1,6 +1,8 @@
 import random
 
 from armor import Armor
+from base import probability
+from enemy.enemy import Enemy
 from entity import Entity
 from weapon import Weapon
 from .player_data import *
@@ -16,6 +18,35 @@ class Player:
         self.entity = Entity(player_type)
         self.armor = Armor(random.choice(list(player_armor.values())))
         self.weapon = Weapon(random.choice(list(player_weapon.values())))
+
+    def fight(self, enemy: Enemy):
+
+
+        while self.entity.health >= 0 or enemy.entity.health >= 0:
+
+            print('ты', self.entity.health)
+            print('враг', enemy.entity.health)
+
+            if probability(self.weapon.hitting_chance):
+                print('ты попал')
+                if self.weapon.damage > enemy.armor.protection:
+                    enemy.entity.health = enemy.entity.health - (self.weapon.damage - enemy.armor.protection)
+                elif self.weapon.damage < enemy.armor.protection:
+                    print('без урона')
+            else:
+                print('ты промазал')
+
+            if probability(enemy.weapon.hitting_chance):
+                print('враг попал')
+                if enemy.weapon.damage > self.armor.protection:
+                    self.entity.health = self.entity.health - (enemy.weapon.damage - self.armor.protection)
+                elif enemy.weapon.damage < self.armor.protection:
+                    print('без урона')
+            else:
+                print('враг промазал')
+
+            print('ты', self.entity.health)
+            print('враг', enemy.entity.health)
 
 # x = Player()
 # x.generate_player()
